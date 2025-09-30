3c8aa7af81babd27b7189fcd6f81d3f0ab72d4d9
Israel A. Possoli <israel.possoli@cargolift.com.br>
2025-09-30 15:56:14 -0300

    Version 0.1.12

--- src/rabbitmq-envelope-message.ts
+++ src/rabbitmq-envelope-message.ts
@@ -5,9 +5,12 @@ import type envelopeContextInterface = require("./context-message.interface.js")
  */
 export interface EnvelopeMessage {
     correlation_id?: string;
-    eventType?: string; // Nome do recurso que está sendo transacionado: driver, trip, cte, nfse
-    source?: envelopeContextInterface.ContextSource;
+    eventType?: string; // Tipo do evento (ex: 'integration.erp.driver')
+    event?: string; // Nome do recurso que está sendo transacionado: driver, trip, cte, nfse
+    action?: string; // Ação realizada: CREATE, UPDATE, DELETE, GET
     method?: string;    // ex: 'CREATE', 'UPDATE', 'DELETE', 'GET'
+    record_id?: string; // ID do recurso que está sendo transacionado
+    source?: envelopeContextInterface.ContextSource;
     timestamp?: string; // ISO 8601 format
     error?: envelopeContextInterface.ContextError;
     [key: string]: any; // Additional dynamic fields
