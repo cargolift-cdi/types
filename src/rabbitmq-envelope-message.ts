6e7be942bdc6cba8bd07eb02dfe23b8f5cdf0929
Israel A. Possoli <israel.possoli@cargolift.com.br>
2025-10-28 18:01:43 -0300

    Version 0.1.16

--- src/rabbitmq-envelope-message.ts
+++ src/rabbitmq-envelope-message.ts
@@ -1,17 +1,17 @@
-import type envelopeContextInterface = require("./context-message.interface.js");
+import type contextMessage = require("./context-message.interface.js");
 
 /**
  * Envelope (metadados) da mensagem publicada.
  */
 export interface EnvelopeMessage {
-    correlation_id?: string;
-    eventType?: string; // Tipo do evento (ex: 'integration.erp.driver')
-    event?: string; // Nome do recurso que está sendo transacionado: driver, trip, cte, nfse
-    action?: string; // Ação realizada: CREATE, UPDATE, DELETE, GET
-    method?: string;    // ex: 'CREATE', 'UPDATE', 'DELETE', 'GET'
-    record_id?: string; // ID do recurso que está sendo transacionado
-    source?: envelopeContextInterface.ContextSource;
-    timestamp?: string; // ISO 8601 format
-    error?: envelopeContextInterface.ContextError;
-    [key: string]: any; // Additional dynamic fields
+  correlation_id?: string;
+  eventType?: string;                     // Tipo do evento (ex: 'integration.erp.driver')
+  event?: string;                         // Nome do recurso que está sendo transacionado: driver, trip, cte, nfse
+  action?: string;                        // Ação realizada: CREATE, UPDATE, DELETE, GET
+  method?: string;                        // ex: 'CREATE', 'UPDATE', 'DELETE', 'GET'
+  record_id?: string;                     // ID do recurso que está sendo transacionado
+  timestamp?: string;                     // ISO 8601 format
+  source?: contextMessage.ContextSource;  // Sistema de origem da mensagem
+  error?: contextMessage.ContextError;    // Detalhes do erro, se aplicável
+  extraData?: { [key: string]: any };     // Qualquer dado extra relevante
 }
